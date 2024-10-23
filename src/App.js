e16fdb60a25c137c6f646a855522db230c6d2739
shreya <70858293+alt-shreya@users.noreply.github.com>
2024-10-23 13:38:31 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import BingoGenerator from "./components/Bingo";
+
+export default function App() {
+  return (
+    <div className="App">
+      <BingoGenerator />
+    </div>
+  );
+}
